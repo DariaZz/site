09f43e5f80cfb4279407c52755b98fead89c30b7
админ <@DariaZz>
2024-06-26 16:00:34 +0800

    Сделала ширным шрифтом надпись и добавила картинку

--- anything/appshop/views.py
+++ anything/appshop/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("Привет! Тут скоро будет магазин")
+    return HttpResponse("<h1>Internet site</h1>Привет! Тут скоро будет магазин <img src= 'https://avatars.mds.yandex.net/i?id=be3988e409cba5545c83ff3b066888914f11f171-12472657-images-thumbs&n=13' >")
