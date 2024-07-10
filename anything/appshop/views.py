c1dfe5d1e32a56094b7f90ba29a8dfe422333512
админ <@DariaZz>
2024-07-10 11:07:39 +0800

    Теперь продукты можно добавлять администратору

--- anything/appshop/views.py
+++ anything/appshop/views.py
@@ -1,9 +1,12 @@
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
 def hello(request):
-    products = []
+    products = Product.objects.all()
     return render(request, "index.html", {
         'products': products
     })
