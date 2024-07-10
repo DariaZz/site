314ca71709a66b9f1d3eeb22bd3647ee63748d7c
админ <@DariaZz>
2024-07-10 09:29:56 +0800

    Повторила за преподователем (Он начал делать отображения продуктов)

--- anything/appshop/views.py
+++ anything/appshop/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello(request):
-    return render(request, "index.html")
+    products = []
+    return render(request, "index.html", {
+        'products': products
+    })
