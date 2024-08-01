9bb5dcdc5c6b0023e9b117f86aca415d3b394505
админ <@DariaZz>
2024-08-01 19:06:25 +0800

    Что-то пошло не так

--- anything/appshop/views.py
+++ anything/appshop/views.py
@@ -1,9 +1,13 @@
-
+import telebot
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from .models import Reviews
 
+
+from .config. import API_TOKEN, CHAT_ID
+
+bot = telebot.TeleBot(API_TOKEN)
 
 # Create your views here.
 def hello(request):
@@ -37,6 +41,12 @@ def pay(request,id):
         adress = request.POST.get('adress')
         phone = request.POST.get('phone')
         print(name, adress, phone)
+        bot.send_message(CHAT_ID, f'''💸 Заказ: {product.name} ({product.price} рублей)
+
+ФИО заказчика: {name}
+Адрес доставки: {address}
+Телефон: {phone}''')
+        return redirect('/success')
 
     return render(request, 'pay.html', {
         'product': product
